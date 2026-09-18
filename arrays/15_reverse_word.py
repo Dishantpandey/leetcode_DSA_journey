1fa115055d11cfa7777366e4609fe0922b6b1b75
DishantPandey <dishantpandey790@gmail.com>
2026-09-18 12:22:03 +0530

    15

---
+++ arrays/15_reverse_word.py
@@ -0,0 +1,7 @@
+text = "Hello World from Python"
+
+# Split the string into words, reverse the list, and join them back
+reversed_text = " ".join(text.split()[::-1])
+
+print(reversed_text)
+# Output: Python from World Hello
